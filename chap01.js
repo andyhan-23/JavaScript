6957eceae952738c8ce0df00b8f5cd4fd3b1ba5b
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-01 19:25:04 +0900

    chap01

---
+++ chap01.js
@@ -0,0 +1,28 @@
+// console.log(sum(3,4));
+
+
+// var sum= function(x,y)
+// {
+//     return x+y;
+// };
+
+// var a= sum(1,2);
+
+// var sum= function(x,y) {
+//     return x+'+' +y+'='(x+y);
+// };
+// var a =1;
+// var outer =function()
+// {
+//     var inner =function()
+//     {
+//         console.log(a);
+//         var a=3;
+//     };
+//     inner();
+//     console.log(a);
+// };
+// outer();
+// console.log(a); 
+
+console.log(andy);
