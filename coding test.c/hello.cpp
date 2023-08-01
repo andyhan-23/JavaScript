ca65b080c6dbe132f371f44b8860e8f02dd755aa
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-01 19:08:18 +0900

    js

---
+++ coding test.c/hello.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int sum=0;
+for(int i=0; i<=10; i++)
+{
+    sum=sum+i;
+}
+
+cout<<sum;
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
