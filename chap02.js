5eab6953fb2dc2822b5db384539c68a387b3e312
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-04 19:22:52 +0900

    chap02

--- chap02.js
+++ chap02.js
@@ -10,12 +10,19 @@
 // outer();
 // console.log(a);
 
-function a(x){
-    console.log(x);
-    var x;
-    console.log(x);
-    var x= 2;
-    console.log(x);
-};
-a(3)
+// function a(x){
+//     console.log(x);
+//     var x;
+//     console.log(x);
+//     var x= 2;
+//     console.log(x);
+// };
+// a(3)
 
+var a=13;
+var b;
+
+b=a;
+
+b=10;
+console.log(a);
