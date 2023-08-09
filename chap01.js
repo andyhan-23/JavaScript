da3aaf57e17d221c3a6271f8c5d115ee6fbf11d4
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-09 12:08:51 +0900

    chap01

--- chap01.js
+++ chap01.js
@@ -1,81 +1,16 @@
-var user=
-{
-    name: 'andy',
-    gender: 'male'
+var a=10;
+var b=a;
+
+b=13;
+console.log(a===b);
+
+var obj1 ={
+    a:10,
+    b:'avvv'
 };
 
-var changeName= function(user, newName)
-{
-    var newUser = user;
-    newUser.name= newName;
-    return newUser;
-};
+var obj2= obj1;
 
-var user2= changeName(user, 'han');
+obj2.a=20;
 
-if(user!==user2)
-{
-    console.log("유저 정보가 변경되었습니다.");
-}
-
-console.log(user.name,user2.name);
-console.log(user===user2);
-
-var user=
-{
-    name: 'andy',
-    gender: 'male'
-};
-
-// var changeName = function(user, newName)
-// {
-//     return{
-//         name: newName,
-//         gender: user.gender
-//     };
-// };
-
-
-var copyObject = function(target)
-{
-    var result ={};
-    for(var prop in target)
-    {
-        result[prop] = target[prop];
-    }
-    return result;
-}
-
-var user2 = copyObject(user);
-user2.name = 'kim';
-
-if(user2!==user)
-{ 
-    console.log('유저 정보가 변경되어습니다.!');
-}
-
-if(user !== user2)
-{
-    console.log(user.name, user2.name);
-    console.log(user===user2);
-}
-
-var user=
-{
-    name: 'andy',
-    extra:{
-        gender: 'male',
-        age: '25', 
-    }
-};
-
-var user2= copyObject(user);
-user2.extra = copyObject(user.extra);
-
-user2.name='han';
-console.log(user.name===user2.name);
-
-user.extra.age = '23';
-console.log(user.extra.age===user2.extra.age);
-
-
+console.log(obj1==obj2);
