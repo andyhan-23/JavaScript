0182283ea65fb64b3f161ec3840a0a242f6f27bb
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-09 14:16:02 +0900

    chap02

--- chap02.js
+++ chap02.js
@@ -1,60 +1,163 @@
-// var a=1;
-// function outer(){
-//     function inner(){
-//         console.log(a);
-//         var a=3;
-//     }
-//     inner();
-//     console.log(a);
-// }
-// outer();
-// console.log(a);
+// // var a=1;
+// // function outer(){
+// //     function inner(){
+// //         console.log(a);
+// //         var a=3;
+// //     }
+// //     inner();
+// //     console.log(a);
+// // }
+// // outer();
+// // console.log(a);
 
 // function a(x){
 //     console.log(x);
 //     var x;
 //     console.log(x);
-//     var x= 2;
+//     var x=2;
 //     console.log(x);
+// }
+// a(1);
+
+// function a(){
+//     var x=1;
+//     console.log(x);
+//     var x;
+//     console.log(x);
+//     var x=2;
+//     console.log(x);
+// }
+// a();
+
+// function a(x){
+//     var x;
+//     var x;
+//     var x;
+//     x=1;
+//     console.log(x);
+//     console.log(x);
+//     x=2;
+//     console.log(x);
+// }
+
+// function a(){
+//     console.log(b);
+//     var b='bbb';
+//     console.log(b);
+//     function b(){}
+//     console.log(b);
+// }
+
+// function a(){
+//     var b;
+//     function b(){}
+//     console.log(b);
+//     b='bbb';
+//     console.log(b);
+//     console.log(b);
+// }
+
+// function a(){
+//     var b;
+//     var b= function b(){}
+//     console.log(b);
+//     b='bbb';
+//     console.log(b);
+//     console.log(b);
+// }
+
+// function a(){ //함수 선언식
+
+// }
+// a(); 
+
+// var b= function (){ // (익명)함수 표현식. 변수명 b가 곧 함수명
+
+// }
+// b();
+
+// var b= function a(){ // (기명)함수 표현식. 변수명은 c, 함수명은 a
+
+// }
+// b(); // 실행 ok
+// a(); // error
+ 
+// console.log(sum(1,2));
+// //console.log(multiply(3,4));
+
+// function sum(a,b){ //함수 선언식
+//     return a+b;
+// }
+
+// var multiply= function(a,b){ // 함수 표현식
+//     return a*b;
+// }
+
+// var sum= function sum(a,b){
+//     return a+b;
+// }
+
+// var multiply;
+
+// console.log(sum(1,2));
+
+// console.log(multiply(3,4));
+
+// multiply = function(a,b){
+//     return a*b;
+// }
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// console.log(sum(4,6));
+
+// console.log(sum(7,8));
+
+// var plus=function(a,b){
+//     return a+"+"+b+"="+(a+b);
+// }
+
+// console.log(plus(3,2));
+
+// var sum = function sum(a,b){
+//     return a+b;
+// }
+
+// var plus;
+
+// console.log(sum(4,6));
+
+// console.log(sum(7,8));
+
+// plus= function(a,b){
+//     return a+"+"+b+"="+(a+b);
+// }
+
+// console.log(plus(3,2));
+
+// var a=1;
+
+// var outer = function(){
+//     var inner = function(){
+//         console.log(a);//undefined
+//         var a=3;
+//     }
+//     inner();
+//     console.log(a);
 // };
-// a(3)
+// outer();
+// console.log(a);
 
-// document.addEventListener("DOMContentLoaded",()=>{
-//     console.log("원하는 문구 적기")
-    
-// })
+function sum(a,b){
+    return a+b;
+}
 
-// function sleep(func, delay) {
-//     const delayUntil = Date.now() + delay;
-    
-//     while (Date.now() < delayUntil);
-//     foo();
-//    }
-   
-//    function foo (){
-//        console.log('foo')
-//    }
-   
-//    function bar () {
-//        console.log('bar')
-//    }
-   
-//    sleep(foo, 3*1000);
-//    bar();
+console.log(sum(4,5));
 
-// function foo(){
-//     console.log('foo');
-// }
+console.log(sum(7,2));
 
-// function bar(){
-//     console.log('bar')
-// }
-
-// setTimeout(foo, 3000);
-// bar();
-
-let a=0.1;
-let b=0.2;
-let sum=a+b;
-
-console.log(sum==0.3);
+var plus = function(a,b){
+    return a+"+"+b+"="+(a+b);
+}
