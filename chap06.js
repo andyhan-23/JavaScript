1e6495b29b715e95d60fd20fcb9fb4dc8290c656
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-17 01:42:09 +0900

    chap06

--- chap06.js
+++ chap06.js
@@ -181,48 +181,204 @@
 
 //부분 함수 구현(2)
 
-Object.defineProperty(window,'_',{
-    value: 'EMPTY_SPACE',
-    writable: false,
-    configurable: false,
-    enumerable: false
-}); 
+// Object.defineProperty(window,'_',{
+//     value: 'EMPTY_SPACE',
+//     writable: false,
+//     configurable: false,
+//     enumerable: false
+// }); 
 
-var partial2= function(){
-    var originalPartialArgs= arguments;
-    var func =originalPartialArgs[0];
-    if(typeof func !=='function' ){
-        throw new Error('첫 번째 인자가 함수가 아닙니다.');
-    }
-    return function(){
-        var partialArgs = Array.prototype.slice.call(originalPartialArgs,1);
-        var restArgs = Array.prototype.slice.call(arguments);
-        for(var i=0; i<partialArgs.length; i++){
-            if(partialArgs[i]=== _){
-                partialArgs[i]=restArgs.shift();
-            }
-        }
-        return func.apply(this, partialArgs.concat(restArgs));
-    };
-};
+// var partial2= function(){
+//     var originalPartialArgs= arguments;
+//     var func =originalPartialArgs[0];
+//     if(typeof func !=='function' ){
+//         throw new Error('첫 번째 인자가 함수가 아닙니다.');
+//     }
+//     return function(){
+//         var partialArgs = Array.prototype.slice.call(originalPartialArgs,1);
+//         var restArgs = Array.prototype.slice.call(arguments);
+//         for(var i=0; i<partialArgs.length; i++){
+//             if(partialArgs[i]=== _){
+//                 partialArgs[i]=restArgs.shift();
+//             }
+//         }
+//         return func.apply(this, partialArgs.concat(restArgs));
+//     };
+// };
 
-var add = function(){
-    var result = 0;
-    for(var i=0; i< arguments.length; i++){
-        result += arguments[i];
-    }
-    return result;
-};
+// var add = function(){
+//     var result = 0;
+//     for(var i=0; i< arguments.length; i++){
+//         result += arguments[i];
+//     }
+//     return result;
+// };
 
-var addPartial = partial2(add, 1,2,_,4,5,_,_,8,9);
-console.log(addPartial(3,6,7,10));
+// var addPartial = partial2(add, 1,2,_,4,5,_,_,8,9);
+// console.log(addPartial(3,6,7,10));
 
-var dog= {
-    name: '강아지',
-    greet:partial2(function(prefix,suffix){
-        return prefix +this.name +suffix;
-    },'왈 왈')
-}
-dog.greet('배고파요!');
+// var dog= {
+//     name: '강아지',
+//     greet:partial2(function(prefix,suffix){
+//         return prefix +this.name +suffix;
+//     },'왈 왈')
+// }
+// dog.greet('배고파요!');
 
 
+/*
+new 연산자로 constructor을 호출하면 instance가 만들어지는데 이 instance의 생략
+가능하 프로퍼티인 __proto__ 는 constructor의 prototype을 참조한다
+*/
+// var instance = new constructor();
+
+// var Person= function(name){
+//     this._name = name;
+// };
+
+// //person 이라는 생성자 함수의 prototype에 getname 이라는 메서드를 지정
+// Person.prototype.getName =function(){
+//     return this._name;
+// };
+
+// var andy= new Person('andy');
+// //console.log(andy.getName());
+
+// var andy = new Person('andy');
+// andy.__proto__._name ='andy';
+// console.log(andy.__proto__.getName());
+
+// var Constructor = function(name){
+//     this.name= name;
+// };
+
+// Constructor.prototype.method1 = function(){};
+// Constructor.prototype.property1= 'constructor prototype property'
+
+// /*new 연산자함 함께 함수를 호출할 경우, 그로부터 생성된 인스턴스에는 숨겨진 프로퍼티인
+// __proto__가 자동으로 생성되며, 이 프로퍼티는 생성자 함수의 prototype 프로퍼티를 참조합니다.*/
+// var instance = new Constructor('instance');
+// console.dir(Constructor);
+// console.dir(instance);
+/*
+생성자 함수의 프로퍼티로는 argument, caller, length, name 그리고 prototype이 있습니다.
+
+*/
+
+// var arr=[1,2]; 
+// console.log(Array.name);
+// // console.dir(arr);
+// // console.dir(Array);
+
+// var NewConstrucotr= function(){
+//     console.log('this is new constructor');
+// };
+
+// var dataTypes = [
+//     1,
+//     'test',
+//     true,
+//     {},
+//     [],
+//     function(){},
+//     /test/,
+//     new Number(),
+//     new String(),
+//     new Boolean(),
+//     new Object(),
+//     new Array(),
+//     new Function(),
+//     new RegExp(),
+//     new Date(),
+//     new Error(),
+// ];
+// dataTypes.forEach(function(d){
+//     d.constructor= NewConstrucotr;
+//     console.log(d.constructor.name ,o)
+// })
+
+// function Car(make, model, year){
+//     this.make = make;
+//     this.model= model;
+// }
+
+// function Bus(make, model, year){
+//     this.make=make;
+//     this.model=model;
+//     this.year = year;
+// }
+
+// var bus = new Bus('132','sdasd', 300);
+// var benz = new Car('mercedes' ,'c-class',1999);
+
+// console.log(benz instanceof Car);
+// console.log(benz instanceof Bus);
+
+// var Person = function(name){
+//     this.name= name;
+// };
+// var p1 = new Person('사람1');
+
+// [p1].forEach(function(p){
+//     console.log(p, p instanceof Person);
+// });
+
+// var Person = function(name){
+//     this.name= name;
+// };
+
+// Person.prototype.getName = function(){
+//     return this.name;
+// }
+
+// var iu = new Person('지금');
+
+// iu.person='지금';
+// iu.getName = function(){
+//     return '바로' + this.name;
+// };
+
+// console.log(iu.getName());
+
+// var Person = function(name){
+//     this.name = name;
+// }
+
+// var Constructor = function(name){
+//     this.name= name;
+// }
+
+// Constructor.prototype.method1 = function(){};
+// Constructor.prototype.property1 = 'constructor prototype property';
+
+// var instance = new Constructor('instance');
+// console.dir(Constructor);
+// console.dir(instance);
+
+// var Person = function(name){
+//     this.name= name;
+// }
+
+// Person.prototype.getName= function(){
+//     return this.name;
+// }
+
+// Person.prototype.name='이지금';
+// var iu= new Person('지금');
+// iu.getName=function(){
+//     return '바로'+this.name;
+// }
+
+// console.log(iu.__proto__.getName.call(iu));
+// console.log(iu.getName());
+
+//   var arr=[1,2];
+// //  Array.prototype.toString.call(arr);
+// //  Object.prototype.toString.call(arr);
+
+// arr.toString();
+
+// arr.toString=function(){
+//     return this.join('_');
+// };
+// console.log(arr.toString());
