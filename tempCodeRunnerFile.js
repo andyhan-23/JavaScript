68af203e1b74b05819a40d600c6c5d4294ed4f2b
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-16 16:00:54 +0900

    mutable

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,11 @@
-(function(){
-//     var a=10;
-//     var intervalId= null;
-//     var inner= function(){
-//         if(++a>=10){
-//             clearInterval(intervalId);
-//         }
-//         console.log(a);
-//     };
-//     intervalId= setInterval(inner, 1000);
-// })();
+// (function(){
+// //     var a=10;
+// //     var intervalId= null;
+// //     var inner= function(){
+// //         if(++a>=10){
+// //             clearInterval(intervalId);
+// //         }
+// //         console.log(a);
+// //     };
+// //     intervalId= setInterval(inner, 1000);
+// // })();
