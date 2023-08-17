1976fde0c9b225dee94725efc0c62a2d7b1d47b7
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-18 02:17:39 +0900

    chap01

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,12 @@
-obj=undefined;
-// console.log(map.get(obj));
+var obj ={a:'a', b:'b', c:'c'};
+
+// for(var i in obj){
+//     console.log(obj[i]);
+// }
+
+// var set = new Set();
+
+// set.add(3);
+// set.add(2);
+// set.add(2);
+// console.log(set);
