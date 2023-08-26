854460aa53a529df92a66e4c6c7e6c73fe92dab2
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-26 15:55:48 +0900

    chap07

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,29 +1,4 @@
-
-// var obj={
-//     a:3,
-// };
-
-// var test={
-//     a:4,
+var add=function(a,b){
+//     return a+b;
 // }
-
-// console.log(test.b);
-
-// var user={
-//     1:'andu',
-//     '2':'ads',
-//     length: 2,
-// };
-// console.log(Object.keys(user));
-
-
-// var obj={};
-// var key= 1;
-// obj[key]='name;';
-
-// console.log(obj);
-
-// var person={
-//     name: 'andy', //프로퍼티
-//     age: 20, //프로퍼티 (age:키, 20:value)
-// }
+// console.log(add(3)); // error: NaN 출력 
