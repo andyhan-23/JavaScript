350343e53178512b5cdcb801728a1ff5ddb1dcdc
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-29 13:35:22 +0900

    Chap07

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,6 @@
-if(myDirection===Direction.UP){
-    console.log("your going up");
-}
+
+// //표준 빌트인 객체를 확장하는 것은 권장하지 않는다.
+// Array.prototype.sum=function(){
+//     return this.reduce((acc, cur)=>acc+cur,0);
+// };
+// console.log([1,2].sum());
