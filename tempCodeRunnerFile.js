5a8e651f83f330b3f6f74cbe15213c461306eac8
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-08-31 02:10:50 +0900

    chap07

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,7 @@
 
-// function multiply(num1,num2){
-//   console.log(arguments);
-//   return num1*num2;
-// }
-
-// console.log(multiply(3,4)); //[Arguments] { '0': 3, '1': 4 } , 12
-// console.log(multiply()); //[Arguments] {}, NaN
-// console.log(multiply(3)); //[Arguments] { '0': 3 }, NaN
-// console.log(multiply(3,4,5)); //[Arguments] { '0': 3, '1': 4, '2': 5 } ,12
-
+//이름과 속성을 갖는 객체
+const person={
+  name: 'andy',
+  address: 'goyang'
+};
+console.log(person);
