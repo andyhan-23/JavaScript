dd401253e033080a2d65fc41e9689a1aae8e0a23
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-09-05 06:54:31 +0900

    chap07

--- chap07.js
+++ chap07.js
@@ -3611,3 +3611,139 @@
 //     console.log(x); //?? 30
 //     console.log(y); //?? 20
 //   }
+
+// function foo(){
+//     var x=1;
+//     return x;
+// }
+// console.log(foo());
+// console.log(x);
+
+// function add(x,y){
+//     return x+y;
+// }
+
+// let test=new add(4,5);
+// console.log(test);
+
+// function Circle(radius){
+//     this.radius=radius;
+//     this.getArea=function(){
+//         return Math.PI * radius;
+//     };
+// };
+
+// const circle1= new Circle(5);
+
+// console.log(circle1.getArea());
+
+// function Circle(radius){
+//     this.radius=radius;
+// }
+
+// Circle.prototype.getArea=function(){
+//     return Math.PI *this.radius;
+    
+// }
+
+// const circle1= new Circle(5);
+
+// console.log(circle1.getArea())
+
+// function Test(str){
+//     this.str=str;
+// }
+
+// const test= new Test();
+// console.log(test.constructor===Test);
+
+// function Person(name){
+//     this.name=name;
+// }
+// const me = new Person('han')p
+
+// const radius ={
+//     radius: 10
+// };
+
+// const circle ={
+//     //프로퍼티: 객체 고유의 상태 데이터
+//     radius: 5,
+//     //메서드: 상태 데이터를 참조하고 조작하는 동작
+//     getDiamter: function(){
+//         // 이 메서드가 자신이 속한 객체의 프로퍼티나 다른 메서드를 참조하려면
+//         // 자신이 속한 객체인 circle을 참조할 수 있어야 한다.
+//         return 2* circle.radius;
+//     }
+// };
+// console.log(circle.getDiamter()); //10
+
+// function Circle(radius){
+//     //이 시점에는 생성자 함수 자신이 생성할 인스턴스를 가리키는 식별자를 알 수 없다.
+//     ????.radius =radius;
+// }
+
+// Circle.prototype.getDiameter=function(){
+//     //이 시점에는 생성자 하함수 자신이 생성할 인스턴스를 가리키는 식별자를 알 수 없다.
+//     return 2 * ????.radius;
+// };
+
+// //생성자 함수로 인스턴스를 생성하려면 먼저 생성자 함수를 정의해야 한다.
+// const circle = new Circle(5);
+
+// //객체 리터럴
+// const circle={
+//     radius: 5,
+//     getDiameter(){
+//         //this는 메서드를 호출한 객체를 가리킨다.
+//         return 2* this.radius;
+//     }
+// };
+
+// console.log(circle.getDiameter()); //10
+
+// //생성자 함수
+// function Circle(radius){
+//     //this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+//     this.radius=radius;
+// }
+
+// Circle.prototype.getDiameter=function(){
+//     //this는 생성자 함수가 
+//     return 2*this.radius;
+// };
+
+// //인스턴스 생성
+// const circle = new Circle(5);
+// console.log(circle.getDiameter()); //10
+
+//this는 어디서든지 참조 가능하다.
+//전역에서 this는 전역 객체 window를 가리킨다.
+// console.log(this);
+
+// function square(number){
+//     //일반 함수 내부에서 this는 전역 개체 window를 가리킨다.
+//     console.log(this); //window
+//     return number*number;
+// }
+// square(2);
+
+// const person={
+//     name: 'han',
+//     getName(){
+//         //메서드 내부에서 this는 메서드를 호출한 객체를 가리킨다.
+//         console.log(this);
+//         return this.name;
+//     }
+// }
+// console.log(person.getName()); //{ name: 'han', getName: [Function: getName] } ,han
+
+// function Person(name){
+//     this.name=name;
+//     //생성자 함수 내부에서 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+//     console.log(this); //Person { name: 'han' }
+// }
+
+// const me = new Person('han');
+
+//this
