5276d82db52b8fba96fe86a301e8a08a2f146bb3
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-09-05 06:54:51 +0900

    chap07

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,9 @@
-
-//함수 정의
-function add(x,y){
-    return x+y;
-};
+const person={
+//     name: 'han',
+//     getName(){
+//         //메서드 내부에서 this는 메서드를 호출한 객체를 가리킨다.
+//         console.log(this);
+//         return this.name;
+//     }
+// }
+// console.log(person.getName()); //{ name: 'han', getName: [Function: getName] } ,han
