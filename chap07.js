4323e8b2124c7c8617db47a064a14d548b628383
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-09-06 04:06:04 +0900

    chap07

--- chap07.js
+++ chap07.js
@@ -3792,3 +3792,92 @@
 //   }
 
 // example();
+
+// function foo(){
+//     console.log("foo's this", this); //window
+//     function bar(){
+//         console.log("bar's this", this); //window
+//     }
+//     bar(); 
+// };
+// foo();
+
+// function foo(){
+//     'use strict';
+
+//     console.log("foo's this:", this); //foo's this: undefined
+//     function bar(){
+//         console.log("bar's this", this); //bar's this undefined
+//     }
+//     bar();
+// };
+// foo();
+
+// //var 키워드로 선언한 전역 변수 value는 전역 객체의 프로퍼티다
+// var value =1;
+// //const 키워드로 선언한 전역 변수 value는 전역 객체의 프로퍼티가 아니다.
+// //const value=1;
+
+// const obj={
+//     value: 100,
+//     foo(){
+//         console.log("foo's this", this); //foo's this { value: 100, foo: [Function: foo] }
+//         console.log("foo's this.value: ", this.value); //foo's this.value:  100
+
+//         //메서드 내에서 정의한 중첩 함수
+//         function bar(){
+//             console.log("bar's this", this); //window
+//             console.log("bar's this.value", this.value);
+//         }
+
+//         //메서드 내에서 정의한 중첩 함수도 일반 함수로 호출되면 중첩 함수 내부의 this에는
+//         //전역 객체가 바인딩된다.
+//         bar();
+//     }
+// };
+// obj.foo();
+
+
+// var value =1;
+
+// const obj={
+//     value: 100,
+//     foo(){
+//         console.log("foo's this", this); //foo's this { value: 100, foo: [Function: foo] }
+//         //콜백 함수 내부의 this에는 전역 객체가 바인딩된다.
+//         setTimeout(function(){
+//             console.log("callback's this: ", this) //window
+//             console.log("callback's this.value: ", this.value); //callback's this.value:  undefined
+//         },100); 
+//     }
+// };
+// obj.foo();
+
+
+
+// var value = 1;
+
+// const obj = {
+
+//   value: 100,
+//   foo() {
+//     console.log("foo's this: ", this); // {value: 100, foo: ƒ}
+//     // 콜백 함수 내부의 this에는 전역 객체가 바인딩된다.
+//     setTimeout(function () {
+//       console.log("callback's this: ", this); // window
+//       console.log("callback's this.value: ", this.value); // 1
+//     }, 100);
+//   }
+// };
+
+// obj.foo();
+
+
+
+// var test=1;
+// function func(){
+//     console.log(this.test);
+// }
+// func();
+
+
