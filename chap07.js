b130a8454fdd27943b8d49b0444268b0ef24739a
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-09-05 18:10:22 +0900

    chap05

--- chap07.js
+++ chap07.js
@@ -3746,4 +3746,49 @@
 
 // const me = new Person('han');
 
-//this
+// //this 바인딩은 함수 호출 방식에 따라 동적으로 결정된다.
+// const foo= function(){
+//     console.dir(this);
+// };
+
+// //동일한 함수도 다양한 방식으로 호출할 수 있다.
+
+// //1. 일반 함수 호출
+// //foo 함수를 일반적인 방식으로 호출
+// //foo 함수 내부의 this는 전역 객체 window를 가리킨다.
+// //foo(); //window
+
+// //2. 메서드 호출
+// //foo 함수를 프로퍼티 값으로 할당하여 호출
+// //foo 함수 내부의 this는 메서드를 호출한 객체 obj를 가리킨다.
+// const obj={foo};
+// obj.foo(); //{ foo: [Function: foo] }
+
+// //3. 생성자 호출
+// //foo 함수를 new연산자와 함께 생성자 함수로 호출
+// //foo 함수 내부의 this는 생성자 함수가 생성한 인스턴스를 가리킨다.
+// new foo(); //foo {}
+
+// //4. Function.prototype.apply/call/bind 메서드에 의한 간접 호출
+// //foo 함수 내부의 this는 인수에 의해 결정된다.
+// const bar= {name: 'bar'};
+// foo.apply(bar); //{ name: 'bar' }
+// foo.call(bar); //{ name: 'bar' }
+// foo.bind(bar)(); //{ name: 'bar' }
+
+// function example() {
+//     var x = 10;
+//     let y = 20;
+  
+//     if (true) {
+//       var x = 30;
+//       let y = 40;
+//       console.log(x) // ?? 30
+//       console.log(y) // ?? 40
+//     }
+  
+//     console.log(x); //?? 30
+//     console.log(y); //?? 20
+//   }
+
+// example();
