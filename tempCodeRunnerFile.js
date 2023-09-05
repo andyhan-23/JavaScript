f41b28d19cf166282e357f6a13dd5f819725c2a6
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-09-06 04:06:08 +0900

    chap07

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,9 +1,17 @@
-const person={
-//     name: 'han',
-//     getName(){
-//         //메서드 내부에서 this는 메서드를 호출한 객체를 가리킨다.
-//         console.log(this);
-//         return this.name;
-//     }
-// }
-// console.log(person.getName()); //{ name: 'han', getName: [Function: getName] } ,han
+
+// var value = 1;
+
+// const obj = {
+
+//   value: 100,
+//   foo() {
+//     console.log("foo's this: ", this); // {value: 100, foo: ƒ}
+//     // 콜백 함수 내부의 this에는 전역 객체가 바인딩된다.
+//     setTimeout(function () {
+//       console.log("callback's this: ", this); // window
+//       console.log("callback's this.value: ", this.value); // 1
+//     }, 100);
+//   }
+// };
+
+// obj.foo();
