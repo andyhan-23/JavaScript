2c3a67a98782d7b505f6ccb40e70a8848f5b906c
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2023-09-07 00:31:01 +0900

    chap07

--- chap07.js
+++ chap07.js
@@ -4191,9 +4191,39 @@
 // }
 // console.log(x); //1
 
-for(let i=0; i<5; i++){
-    let fuc= function(){
-        console.log(i);
-    }
-    fuc();
+// const x=1;
+
+// function outerFunc(){
+//     const x=10;
+//     function innerFunc(){
+//         console.log(x); //10
+//     }
+//     innerFunc();
+// }
+// outerFunc();
+
+// const x=1;
+
+// function outerFunc(){
+//     const x=10;
+//     innerFunc();
+// }
+
+// function innerFunc(){
+//     console.log(x); //1
+// }
+// outerFunc();
+
+const x=1;
+
+function foo(){
+    const x=10;
+    bar();
+}
+
+function bar(){
+    console.log(x);
 };
+
+foo(); //1
+bar(); //1
